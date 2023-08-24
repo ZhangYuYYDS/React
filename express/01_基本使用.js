123eebcaf19eb90babd40024952ea759580d0bad
zhangyu <1015371461@qq.com>
2023-08-24 11:31:38 +0800

    基本使用

--- express/01_基本使用.js
+++ express/01_基本使用.js
@@ -3,7 +3,17 @@ const express = require('express')
 // 1. 创建express服务器
 const app = express()
 
+// 客户端访问URL：/home和/login
+app.post('/login', (req, res) => {
+    // 处理login请求
+    res.end('Hello login')
+})
+
+app.get('/home', (req, res) => {
+    res.end('Hello home')
+})
+
 // 2. 启动服务器，并且监听端口
-app.listen(8000, () => {
+app.listen(9000, () => {
     console.log("服务器启动成功")
 })

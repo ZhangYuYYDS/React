55e7eb5db941f24b16fb8c43812b1347f2813b9f
zhangyu <1015371461@qq.com>
2023-10-25 21:24:37 +0800

    购物车做法一

---
+++ React/04_购物车阶段案例/data.js
@@ -0,0 +1,30 @@
+const books = [
+    {
+        id: 1,
+        name: '《算法导论》',
+        data: '2006-9',
+        price: 85.0,
+        count: 1,
+    },
+    {
+        id: 2,
+        name: '《UNIX编程艺术》',
+        data: '2006-9',
+        price: 59.0,
+        count: 1,
+    },
+    {
+        id: 3,
+        name: '《编程珠玑》',
+        data: '2006-9',
+        price: 39.0,
+        count: 1,
+    },
+    {
+        id: 4,
+        name: '《代码大全》',
+        data: '2006-9',
+        price: 128.0,
+        count: 1,
+    },
+];

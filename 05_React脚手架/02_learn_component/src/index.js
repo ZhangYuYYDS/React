a1d37d382546ff4b83e33a62e697490c15f243ac
zhangyu <1015371461@qq.com>
2023-12-03 22:56:19 +0800

    组件化

--- 05_React脚手架/02_learn_component/src/index.js
+++ 05_React脚手架/02_learn_component/src/index.js
@@ -1,11 +1,9 @@
 import ReactDOM from 'react-dom/client';
 // import App from './01_类组件和函数组件/AppClass';
-import App from './02_组件生命周期函数/App';
-// import AppFun from './AppFun';
+// import App from './02_组件生命周期函数/App';
+import App from './03_组件开发的嵌套关系/App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-// 类组件
+
 root.render(<App></App>);
-// 函数组件
-// root.render(<AppFun></AppFun>);
